ac62424e67d800e0fc50d5c8e805e779cd8f4da4
TheInfamousGrim <finchergeorge1@gmail.com>
2022-12-15 20:31:12 +0000

    Feat: add state for meme form and meme data
    Refactored the code to add state for the form inputs as well as the
    random meme img and stored that state in the form of an object.
    
    Stored the meme data as state for some reason. I don't think we should
    be storing the data as state...

--- src/components/MemeForm/index.jsx
+++ src/components/MemeForm/index.jsx
@@ -4,25 +4,35 @@ import React, {useState} from 'react';
 import memeData from '../../data/memesData';
 
 export default function MemeForm() {
-    const [memeImageURL, setMemeImageURL] = useState(memeData.data.memes[0].url)
+    const [allMemeImages, setAllMemeImages] = useState(memeData);
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: allMemeImages.data.memes[0].url
+    });
 
-    const handleNewMeme = (e) => {
+    const getMemeImage = (e) => {
         e.preventDefault();
         // Get a random index from the array of meme data
-        const randomIndex = Math.floor(Math.random() * (memeData.data.memes.length - 1));
-        setMemeImageURL(memeData.data.memes[randomIndex].url);
+        const memeArray = allMemeImages.data.memes;
+        const randomIndex = Math.floor(Math.random() * (memeArray.length - 1));
+        const memeURL = memeArray[randomIndex].url;
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: memeURL
+        }));
     }
 
     return (
         <main className="meme-generator-container">
-            <form className="meme-generator-form" onSubmit={handleNewMeme}>
+            <form className="meme-generator-form" onSubmit={getMemeImage}>
                 <div className="meme-generator-form__text-inputs">
                     <input type="text" name="top-text" id="topText" className="top-text meme-inputs" placeholder="top text..." />
                     <input type="text" name="bottom-text" id="bottomText" className="bottom-text meme-inputs" placeholder="bottom text..." />
                 </div>
                 <button type="submit" className="meme-generator-form__button">Get a new meme image 🖼️</button>
             </form>
-            <img src={memeImageURL} alt="A lovely juicy meme" className='meme-img-template'/>
+            <img src={meme.randomImage} alt="A lovely juicy meme" className='meme-img-template'/>
         </main>
     )
 }
