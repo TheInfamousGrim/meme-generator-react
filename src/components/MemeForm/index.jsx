7239f27f6e8d820dbe47455bfb70687326b4a402
TheInfamousGrim <finchergeorge1@gmail.com>
2022-12-15 19:28:45 +0000

    Feat: generate random meme img
    Added some files that has data on some of the top meme img templates.
    
    Used that data to create a function that will generate a random img
    from the meme data using a url that is stored in the data.

--- src/components/MemeForm/index.jsx
+++ src/components/MemeForm/index.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, {useState} from 'react';
+
+// Data
+import memeData from '../../data/memesData';
 
 export default function MemeForm() {
+    const [memeImageURL, setMemeImageURL] = useState(memeData.data.memes[0].url)
+
+    const handleNewMeme = (e) => {
+        e.preventDefault();
+        // Get a random index from the array of meme data
+        const randomIndex = Math.floor(Math.random() * (memeData.data.memes.length - 1));
+        setMemeImageURL(memeData.data.memes[randomIndex].url);
+    }
+
     return (
         <main className="meme-generator-container">
-            <form className="meme-generator-form">
+            <form className="meme-generator-form" onSubmit={handleNewMeme}>
                 <div className="meme-generator-form__text-inputs">
                     <input type="text" name="top-text" id="topText" className="top-text meme-inputs" placeholder="top text..." />
                     <input type="text" name="bottom-text" id="bottomText" className="bottom-text meme-inputs" placeholder="bottom text..." />
                 </div>
                 <button type="submit" className="meme-generator-form__button">Get a new meme image 🖼️</button>
             </form>
+            <img src={memeImageURL} alt="A lovely juicy meme" className='meme-img-template'/>
         </main>
     )
 }
